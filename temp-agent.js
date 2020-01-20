cc485e6e07c599ee093697b3aa34842674c140ac
Hypriot Pirate <costa@turingpi.com>
2020-01-20 19:58:34 +0000

    exec sh fix

--- temp-agent.js
+++ temp-agent.js
@@ -10,7 +10,8 @@ const port = 5000;
 const route_404 = '404 - Page not found';
 app.use(pretty({ query: 'pretty' }));
 // Execute shell command
-function sh_exec(command) {
+
+/*function sh_exec(command) {
     return new Promise((resolve, reject) => {
      exec(command, (error, stdout, stderr) => {
          if (error) console.warn(error);
@@ -20,8 +21,21 @@ function sh_exec(command) {
          }
          else resolve(stdout);
         });
-    }); 
+    });
+}*/
+
+function sh_exec(cmd) {
+ //const exec = require('child_process').exec;
+ return new Promise((resolve, reject) => {
+  exec(cmd, (error, stdout, stderr) => {
+   if (error) {
+    console.warn(error);
+   }
+   resolve(stdout? stdout : stderr);
+  });
+ });
 }
+
 // API route for getting temperature of individual node
 app.get('/api/get-node-temp', async (_req, resp) => {
     var podName =  (await sh_exec('hostname')).trim();
@@ -32,17 +46,18 @@ app.get('/api/get-node-temp', async (_req, resp) => {
 });
 // API route for getting temperature of the cluster
 app.get('/api/get-cluster-temp', async (req, resp) => {
-    var podIpList = (await sh_exec("kubectl get pods -n pi-temp -o wide 2>/dev/null | grep pi-temp | awk '{print $6}'")).split("\n");
+    var podIpList = (await sh_exec("nslookup pi-temp-headless 2>/dev/null | grep Address | awk '{print $2}'")).split("\n");
     var podIpList = podIpList.filter((item) => { return item.trim() != '' });
     var result = [];
-for (let i = 0; i < podIpList.length; i++){
+    console.log("IP LIST = ", podIpList);
+    for (let i = 1; i < podIpList.length; i++){
         await rp(`http://${podIpList[i]}:5000/api/get-node-temp`).then(body => {
             result.push(JSON.parse(body));
         }).catch(err => {
             console.log(err);
         });
     }
-resp.json(result);
+    resp.json(result);
 });
 // All other routes return 404
 app.get('*', function(_req, res){
